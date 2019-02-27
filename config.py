260a5a634cc37a928b13d56d008514e79c099bf7
Sprottenfraulein <mslaimagulbe@gmail.com>
2019-02-27 13:12:38 +0300

    config.py modified to work with hosting environment variables

--- config.py
+++ config.py
@@ -1,9 +1,11 @@
 # configuration file
+import os
+
 telbot_config = {
-    'bot_token': '',
-    'db_host': '',
-    'db_user': '',
-    'db_password': '',
-    'db_name': '',
+    'bot_token': os.environ['TEL_BOT_TOKEN'],
+    'db_host': 'localhost',
+    'db_user': os.environ['MYSQL_USER'],
+    'db_password': os.environ['MYSQL_PASSWORD'],
+    'db_name': os.environ['MYSQL_DATABASE'],
     'db_charset': 'utf8mb4'
 }
